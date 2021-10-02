a2860516419801e36ed1dc0f6b4c29ebbd18ddf2
Caity Gossland <caitygossland@gmail.com>
2021-10-01 18:29:57 -0700

    FocusTime React Native app (moved from Expo Snack)

---
+++ src/components/Countdown.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+import { fontSizes, spacing } from '../utils/sizes';
+import { colors } from '../utils/colors';
+
+const minToMillis = (min) => min * 1000 * 60;
+
+const formatTime = (time) => (time < 10 ? `0${time}` : time);
+
+export const Countdown = ({ minutes = 0.1, isPaused, onProgress, onEnd }) => {
+  const interval = React.useRef(null);
+  const [millis, setMillis] = useState(minToMillis(minutes));
+
+  const minutesLeft = Math.floor(millis / 1000 / 60) % 60;
+  const secondsLeft = Math.floor(millis / 1000) % 60;
+
+  const countDown = () => {
+    setMillis((time) => {
+      if (time === 0) {
+        clearInterval(interval.current);
+        return time;
+      }
+      const timeLeft = time - 1000;
+      return timeLeft;
+    });
+  };
+
+  useEffect(() => {
+    setMillis(minToMillis(minutes));
+  }, [minutes]);
+
+  useEffect(() => {
+    onProgress(millis / minToMillis(minutes));
+    if (millis === 0) {
+      onEnd();
+    }
+  }, [millis]);
+
+  useEffect(() => {
+    if (isPaused) {
+      if (interval.current) clearInterval(interval.current);
+      return;
+    }
+    interval.current = setInterval(countDown, 1000);
+
+    return () => clearInterval(interval.current);
+  }, [isPaused]);
+
+  return (
+    <Text style={styles.text}>
+      {formatTime(minutesLeft)}:{formatTime(secondsLeft)}
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: fontSizes.xxxl,
+    fontWeight: 'bold',
+    color: colors.white,
+    padding: spacing.lg,
+    backgroundColor: colors.royalBlue,
+  },
+});
